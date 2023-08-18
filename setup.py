27007b9078d3abc4c61174211331d6ce8a49185a
ibriquem <iwan.briquemont@nokia.com>
2023-08-18 14:37:39 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ test_requirements = ['pytest']
 
 setup(
     name='pytest_exploratory',
-    version='0.8.0',
+    version='0.9.0',
     description='Interactive console for pytest.',
     url='https://github.com/nokia/pytest-exploratory',
     author='Iwan Briquemont',
